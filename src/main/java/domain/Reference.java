2f3b3fe0e89e084ee05f65d84f71c12443a1defc
Hansformer <heinomikael@gmail.com>
2017-04-06 21:34:12 +0300

    Pohjustus Reference Superclassille

--- src/main/java/domain/Reference.java
+++ src/main/java/domain/Reference.java
@@ -1,5 +1,38 @@
 package domain;
 
 public class Reference {
-    
+
+    /**
+     * TODO: Superclass for references
+     * Data structures for storing all the fields and their values conveniently
+     * for easy access and control.
+     * Should contain mock functions too.
+     * Fill requiredFields in each Class Constructor separately with correct
+     * values.
+     */
+    /*
+    private ArrayList<String> requiredFields;
+    private HashMap<String, String> fields;
+
+    public void setField(String field, String value) {
+        fields.put(field, value);
+    }
+
+    public String getField(String field) {
+        if (fields.get(field) == NULL) {
+            return NULL;
+        } else
+    }
+
+    public List getRequiredFields() {
+        return requiredFields;
+    }
+
+    public boolean hasRequiredFields() {
+        for (String s : requiredFields) {
+            if (!fields.hasKey(s)) return false;
+        }
+        return true;
+    }
+    */
 }
