10f5831715b734a50bf848d18d4cae61e79aa907
Hansformer <heinomikael@gmail.com>
2017-04-20 18:23:03 +0300

    Protovalidator

---
+++ src/main/java/logic/Validator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+public class Validator {
+    
+    public final String regexString    = "^[a-zA-Z0-9!@#$%&*()_+=|<>?{}\\s\\[\\]~-]*$";
+    public final String regexNumeric      = "[0-9]+";
+    // public final String regexMonth     = "";
+    
+    public boolean checkTitle() {
+        return true;
+    }
+    
+    public boolean checkAuthor() {
+        return true;
+    }
+    
+    public boolean checkEditor() {
+        return true;
+    }
+    
+    public boolean checkPublisher() {
+        return true;
+    }
+    
+    public boolean checkBookTitle() {
+        return true;
+    }
+    
+    public boolean checkValue(String regex, String value) {
+        return value.matches(regex);
+    }
+    
+}
